da9d8ba660bbec66596cd347be813c58a77a725d
luis001126 <luisalexisrojasrondan@gmail.com>
2023-11-14 16:24:14 -0600

    3do commit
    tabla 2, extrapolacion, GUI contains

--- GUI.py
+++ GUI.py
@@ -17,6 +17,10 @@ def Referencias():
     mBox.showinfo('Referencias', 'Este programa fue realizado por Luis Alexis Rojas Rondan'
                                  ' para mas informacion ponerse en contacto por via email: luisalexisrojasrondan@gmail.com'
                                  ' o al numero de telefono +53 59484201')
+
+def Funcionamiento():
+    mBox.showinfo('Funcionamiento','Aqui se muestra el funcionamiento')
+
 def GetValorsI():
     x0 = entrada_x1.get()
     x1 = entrada_x3.get()
@@ -38,7 +42,7 @@ barra_de_menu: Menu = Menu(ventana)
 ventana.config(menu=barra_de_menu)
 
 opc1 = Menu(barra_de_menu, tearoff=0)
-opc1.add_command(label='Fucionamiento')
+opc1.add_command(label='Fucionamiento', command=Funcionamiento)
 opc1.add_separator()
 opc1.add_command(label='Referencias', command=Referencias)
 barra_de_menu.add_cascade(label='Ayuda', menu=opc1)
@@ -106,6 +110,50 @@ texto_xE = Label(tabla2, text='valores asociados a X')
 texto_xE.place(x=0, y=50)
 texto_yE = Label(tabla2, text='valores asociados a Y')
 texto_yE.place(x=150, y=50)
+
+entrada_xE1: IntVar = IntVar()
+entrada_xE1.set('')
+entrada_xE1_entrar = Entry(tabla2, textvariable=entrada_xE1, justify=CENTER)
+entrada_xE1_entrar.place(x=0, y=80)
+
+entrada_xE2 = IntVar()
+entrada_xE2.set('')
+entrada_xE2_entrar = Entry(tabla2, textvariable=entrada_xE2, justify=CENTER)
+entrada_xE2_entrar.place(x=0, y=100)
+
+
+entrada_xE3 = IntVar()
+entrada_xE3.set('')
+entrada_xE3_entrar = Entry(tabla2, textvariable=entrada_xE3, justify=CENTER)
+entrada_xE3_entrar.place(x=0, y=120)
+
+entrada_xE4 = IntVar()
+entrada_xE4.set('')
+entrada_xE4_entrar = Entry(tabla2, textvariable=entrada_xE4, justify=CENTER)
+entrada_xE4_entrar.place(x=0, y=140)
+
+entrada_yE1 = IntVar()
+entrada_yE1.set('')
+entrada_yE1_entrar = Entry(tabla2, textvariable=entrada_yE1, justify=CENTER)
+entrada_yE1_entrar.place(x=150, y=80)
+
+
+entrada_yE2 = IntVar()
+entrada_yE2.set('')
+entrada_yE2_entrar = Entry(tabla2, textvariable=entrada_yE2, justify=CENTER)
+entrada_yE2_entrar.place(x=150, y=100)
+
+
+entrada_yE3 = IntVar()
+entrada_yE3.set('')
+entrada_yE3_entrar = Entry(tabla2, textvariable=entrada_yE3, justify=CENTER)
+entrada_yE3_entrar.place(x=150, y=120)
+
+entrada_yE4 = IntVar()
+entrada_yE4.set('')
+entrada_yE4_entrar = Entry(tabla2, textvariable=entrada_yE4, justify=CENTER)
+entrada_yE4_entrar.place(x=150, y=140)
+
 # --------------------------------------------
 tabla3 = ttk.Frame(tabla_control, bg='gray')
 tabla_control.add(tabla3, text='Logica')
@@ -118,5 +166,14 @@ boton_salir.place(x=390, y=360, width=50, height=25)
 # --------------------------------------------
 boton_calcularI = Button(tabla1, text='Calcular',command=GetValorsI)
 boton_calcularI.place(x=300, y=200, width=50, height=25)
+
+boton_calcularI = Button(tabla2, text='Calcular')
+boton_calcularI.place(x=300, y=200, width=50, height=25)
+#---------------------------------------------------
+botonradial1=Radiobutton(tabla2,text='Deshabilitar')
+botonradial1.place(x=300,y=80)
+
+botonradial2=Radiobutton(tabla2,text='Deshabilitar')
+botonradial2.place(x=300,y=140)
 
 ventana.mainloop()
